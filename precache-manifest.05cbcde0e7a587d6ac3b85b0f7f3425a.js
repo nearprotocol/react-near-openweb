04a729daa992216acd9da0b3d7e3ac89c71b73ab
Evgeny Kuzyakov <ek@nearprotocol.com>
2020-02-21 12:28:21 -0800

    Updates

---
+++ precache-manifest.05cbcde0e7a587d6ac3b85b0f7f3425a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b907f7006ff6c98dd8ba7e7c4bd79cff",
+    "url": "/react-near-openweb/index.html"
+  },
+  {
+    "revision": "ad9fec328409c6904153",
+    "url": "/react-near-openweb/static/css/main.a2b7b0b6.chunk.css"
+  },
+  {
+    "revision": "1493a55788367eb930db",
+    "url": "/react-near-openweb/static/js/2.71555af8.chunk.js"
+  },
+  {
+    "revision": "d50253769daacdd9ab353251af670f81",
+    "url": "/react-near-openweb/static/js/2.71555af8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ad9fec328409c6904153",
+    "url": "/react-near-openweb/static/js/main.5d8574e5.chunk.js"
+  },
+  {
+    "revision": "b4e0453dbf2500c6fe7d",
+    "url": "/react-near-openweb/static/js/runtime-main.34c13cb3.js"
+  }
+]);
